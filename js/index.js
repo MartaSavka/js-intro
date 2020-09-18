1bda78b4d5379e55f4871d77964b765ba3984bde
MartaSavka <martasavka@gmail.com>
2020-09-18 08:52:10 +0000

    Sample code

---
+++ js/index.js
@@ -0,0 +1,24 @@
+//const message = 'Hello World';
+///alert('message');
+
+//const sum = 2 + 2;
+//alert(sum);
+
+const age = parseInt(prompt('Enter your age'));
+alert ('Your age is' + age)
+
+const nextAge = age + 1;
+alert ('Next year you will be ' + nextAge);
+
+
+if (age < 18) {
+    alert ('You are underaged!');
+} else {
+    alert ('You are adult!!');
+}
+
+function add(a, b){
+    const sum = a + b;
+    return sum;
+}
+
